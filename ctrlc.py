a731c30b7eb724efc3f7daf1a9b13a4dd2054159
doctaphred <doctaphred@gmail.com>
2016-01-21 16:58:40 -0500

    Generalize the signal-handling context manager
    Removes goofy module-import side effect, allows handling arbitrary
    signals, and replaces the state-altering register_cleanup_prompt
    function with a context manager. Also improves function names.

--- ctrlc.py
+++ ctrlc.py
@@ -4,58 +4,55 @@ from contextlib import contextmanager
 from textwrap import dedent
 
 
-_original_sigint = signal.getsignal(signal.SIGINT)
-
-
 @contextmanager
-def original_sigint():
-    """Set SIGINT to its original value, then back to whatever it was set to.
-
-    "original value" is the value defined when this module is loaded.
-    """
-    prev_sigint = signal.getsignal(signal.SIGINT)
-    signal.signal(signal.SIGINT, _original_sigint)
+def handle(signalnum, temp_handler=signal.SIG_DFL):
+    """Temporarily set the signal's handler function."""
+    prev_handler = signal.getsignal(signalnum)
+    signal.signal(signalnum, temp_handler)
     try:
         yield
     finally:
-        signal.signal(signal.SIGINT, prev_sigint)
+        signal.signal(signalnum, prev_handler)
 
 
-def register_cleanup_prompt(cleanup_function):
+def sysexit(signum, frame):
+    print("\nExiting immediately. Please remember to clean up after yourself!")
+    sys.exit(1)
+
+
+def prompt(cleanup_function):
     """Give the user a decision prompt when ctrl+C is pressed."""
     def cleanup_prompt(signum, frame):
         # Allow the user to exit immediately by
         # pressing ctrl+C again while in this block.
-        with original_sigint():
-            try:
-                choice = input(dedent("""
-                    You pressed ctrl+C. What would you like to do?
-                        - to exit immediately, press ctrl+C again
-                        - to clean up and exit gracefully, enter 'c' or 'cleanup'
-                        - to resume operation, press enter
-                    (cleanup?): """
-                    ))
-                if choice and 'cleanup'.casefold().startswith(choice.casefold()):
-                    print('Cleaning up...')
-                    cleanup_function()
-                    print('Done cleaning up; exiting.')
-                    sys.exit(1)
-            except KeyboardInterrupt:
-                print("\nExiting immediately. Please remember to clean up after yourself!")
+        with handle(signal.SIGINT, sysexit):
+            choice = input(dedent("""
+                You pressed ctrl+C. What would you like to do?
+                    - to exit immediately, press ctrl+C again
+                    - to clean up and exit gracefully, enter 'c' or 'cleanup'
+                    - to resume operation, press enter
+                (cleanup?): """
+                ))
+            if choice and 'cleanup'.casefold().startswith(choice.casefold()):
+                print('Cleaning up...')
+                cleanup_function()
+                print('Done cleaning up; exiting.')
                 sys.exit(1)
         print('Continuing...')
 
-    signal.signal(signal.SIGINT, cleanup_prompt)
+    return cleanup_prompt
 
 
 if __name__ == '__main__':
     import time
 
+    def main():
+        while True:
+            print('.')
+            time.sleep(1)
+
     def cleanup():
         print('....................')
 
-    register_cleanup_prompt(cleanup)
-
-    while True:
-        print('.')
-        time.sleep(1)
+    with handle(signal.SIGINT, prompt(cleanup)):
+        main()
