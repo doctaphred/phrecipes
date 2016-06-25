b9137dcaf4e76fd7013bc1b7bb96e7629fd36b7e
Frederick Wagner <doctaphred@gmail.com>
2016-06-25 18:46:46 -0400

    Add some url utils

---
+++ urls.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+from urllib import urlencode
+from urlparse import ParseResult, urlparse, urlunparse
+
+
+def url(scheme='', netloc='', path='', params='', query=(), fragment=''):
+    """Construct a URL from individual components.
+
+    <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
+
+    Args:
+        schema: "http", "https", etc
+        netloc: aka "host"
+        path: "/path/to/resource". Leading slash is automatic.
+        params: Rarely used; you probably want query.
+        query: May be a dict or a sequence of 2-tuples.
+        fragment: Position on page; usually used only by the browser.
+    """
+    return urlunparse((scheme, netloc, path, params, urlencode(query), fragment))
+
+
+def alter_url(url, **components):
+    """
+    >>> alter_url('http://placekitten.com', path='200/300')
+    'http://placekitten.com/200/300'
+    """
+    original = urlparse(url)._asdict()
+    original.update(components)
+    return ParseResult(**original).geturl()
+
+
+def query_string(**kwargs):
+    return '?' + urlencode(kwargs)
