88317dd614cae8acf6f2a997239a620ad020466c
Frederick Wagner <doctaphred@gmail.com>
2016-08-06 10:05:21 -0400

    Magic!

--- funccools.py
+++ funccools.py
@@ -1,3 +1,12 @@
+def call(*args, **kwargs):
+    def decorator(callable):
+        return callable(*args, **kwargs)
+    return decorator
+
+
+magic = call()
+
+
 def combine(*funcs):
     def multifunc(*args, **kwargs):
         return [func(*args, **kwargs) for func in funcs]
