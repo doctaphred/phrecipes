3aa8a74d43bda55964f24ca205ba7b61ea352569
Frederick Wagner <doctaphred@gmail.com>
2016-08-11 22:15:32 -0400

    Improve rate limiter

--- ratelimit.py
+++ ratelimit.py
@@ -1,67 +1,43 @@
 from collections import deque
-from functools import wraps
+from functools import partial, wraps
 import time
 
 
-class RateLimiter:
-    """
-    >>> from unittest.mock import Mock
-    >>> from itertools import count
+class RateLimited(RuntimeError):
+    pass
 
-    >>> func = Mock()
-    >>> clock = count().__next__
-    >>> limited = RateLimiter(1, 1, func, clock=clock)
 
-    >>> len(limited.times_called)
-    0
-    >>> limited.below_limit()
-    True
-    >>> func.call_count
-    0
+class RateLimit:
 
-    >>> _ = limited()
-    >>> len(limited.times_called)
-    1
-    >>> limited.below_limit()
-    False
-    >>> func.call_count
-    1
-
-    >>> _ = limited()
-    >>> len(limited.times_called)
-    1
-    >>> limited.below_limit()
-    False
-    >>> func.call_count
-    2
-    """
-
-    def __init__(self, calls_per_interval, interval, func,
-                 fallback=None, clock=time.perf_counter):
-        self.calls_per_interval = calls_per_interval
-        self.times_called = deque(maxlen=calls_per_interval)
+    def __init__(self, max_calls, interval, clock=time.perf_counter):
+        self.max_calls = max_calls
         self.interval = interval
-        self.func = func
-        self.fallback = fallback
         self.clock = clock
+        self.calls = deque(maxlen=max_calls)
 
-    def below_limit(self):
-        if len(self.times_called) < self.calls_per_interval:
-            return True
-        return self.clock() - self.times_called[-1] > self.interval
+    def cooldown(self, timestamp=None):
+        """Return the remaining time until the rate limit resets."""
+        if timestamp is None:
+            timestamp = self.clock()
 
-    def __call__(self, *args, **kwargs):
-        if self.below_limit():
-            self.times_called.append(self.clock())
-            return self.func(*args, **kwargs)
-        elif self.fallback is not None:
-            return self.fallback(*args, **kwargs)
-        else:
-            return None
+        if len(self.calls) < self.max_calls:
+            return 0.0
 
+        current_interval = timestamp - self.calls[-1]
+        if current_interval > self.interval:
+            return 0.0
 
-def rate_limit(calls_per_interval, interval, **kwargs):
-    def decorator(func):
-        return wraps(func)(RateLimiter(
-            calls_per_interval, interval, func, **kwargs).__call__)
-    return decorator
+        return self.interval - current_interval
+
+    def attempt(self, func, *args, **kwargs):
+        """Call the function, or raise RateLimited."""
+        timestamp = self.clock()
+        cooldown = self.cooldown(timestamp)
+        if cooldown:
+            raise RateLimited(cooldown)
+        self.calls.append(timestamp)
+        return func(*args, **kwargs)
+
+    def __call__(self, func):
+        """Allow this object to be used as a function wrapper or decorator."""
+        return wraps(func)(partial(self.attempt, func))
