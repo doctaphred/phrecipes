766996aa044632297778570e4d64a4b4606925eb
Frederick Wagner <doctaphred@gmail.com>
2016-08-12 17:53:25 -0400

    Add odict.__call__

--- odict.py
+++ odict.py
@@ -1,3 +1,19 @@
+"""
+>>> odict['a': 1, 'b': 2, 'c': 3]
+OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+
+>>> odict['a': 1]
+OrderedDict([('a', 1)])
+
+>>> odict('a', 1, 'b', 2, 'c', 3)
+OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+
+>>> odict('a', 1)
+OrderedDict([('a', 1)])
+
+>>> odict()
+OrderedDict()
+"""
 from collections import OrderedDict
 
 from magic import magic
@@ -5,17 +21,7 @@ from magic import magic
 
 @magic
 class odict:
-    """OrderedDict builder with nice syntax.
-
-    >>> odict['a': 1, 'b': 2, 'c': 3]
-    OrderedDict([('a', 1), ('b', 2), ('c', 3)])
-
-    >>> odict[]
-    OrderedDict([])
-
-    >>> odict['a': 1]
-    OrderedDict([('a', 1)])
-    """
+    """OrderedDict builder with nice syntax."""
     def __getitem__(self, key):
         try:
             # Assume key is a tuple of slices
@@ -23,4 +29,8 @@ class odict:
         except TypeError:
             # Assume key is a single slice
             items = [(key.start, key.stop)]
+        return OrderedDict(items)
+
+    def __call__(self, *kv):
+        items = zip(kv[::2], kv[1::2]) if kv else []
         return OrderedDict(items)
