b7d819e3a759ff42f6edf23bfe9e3826fe0f9c97
Frederick Wagner <doctaphred@gmail.com>
2020-10-14 17:49:29 -0400

    Add join.py

---
+++ join.py
@@ -0,0 +1,52 @@
+from functools import partial
+
+
+class join:
+    r"""Wrap a generator with str.join.
+
+        >>> @join.on('\n')
+        ... def lines(**kwargs):
+        ...     for k, v in kwargs.items():
+        ...         yield f'{k}={v}'
+        >>> print(lines(foo='bar', ayy='lmao'))
+        foo=bar
+        ayy=lmao
+
+    Automatically coerces yielded values to strings:
+
+        >>> join(range)(10)
+        '0123456789'
+
+    Also works as a method decorator:
+
+        >>> class Thing:
+        ...     def __init__(self, **kwargs):
+        ...         vars(self).update(kwargs)
+        ...
+        ...     @join.on('\n')
+        ...     def __repr__(self):
+        ...         yield type(self).__name__
+        ...         for name, value in vars(self).items():
+        ...             yield f'  {name}: {value}'
+
+        >>> Thing(a=1, b=2)
+        Thing
+          a: 1
+          b: 2
+
+    """
+    def __init__(self, func, sep=''):
+        self.func = func
+        self.sep = sep
+
+    @classmethod
+    def on(cls, sep):
+        return partial(cls, sep=sep)
+
+    def __call__(*args, **kwargs):
+        self, *args = args
+        return self.sep.join(map(str, self.func(*args, **kwargs)))
+
+    def __get__(self, instance, owner):
+        """Works as a method decorator too!"""
+        return partial(self, instance)
