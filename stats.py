a5036ec2f7bade16f978de3d97ab46ea18ca976d
Frederick Wagner <frederick@frederickwagner.net>
2021-07-17 17:55:25 -0400

    Overhaul stats module

--- stats.py
+++ stats.py
@@ -1,217 +1,19 @@
+from collections import namedtuple
+from math import isclose
 import warnings
 
 
-def mean(sum, count):
-    """x̄"""
-    return sum / count
-
-
-def variance(ssdm, count):
-    """Unbiased sample variance, using Bessel's correction (s²)."""
-    try:
-        return ssdm / (count - 1)
-    except ZeroDivisionError:
-        return 0
-
-
-def stdev(ssdm, count):
-    """s"""
-    return variance(ssdm, count) ** 0.5
-
-
-x̄, s, s2 = mean, stdev, variance
-
-
-def indented(obj, template='    {}'):
-    return template.format(obj)
-
-
-def indented_lines(*lines, template='    {}', sep='\n'):
-    return sep.join(indented(line, template) for line in lines)
-
-
-class Stats:
-    """Calculate running statistics of a sequence of samples.
-
-    Uses Welford's method for computing variance.
-
-    See https://www.johndcook.com/blog/standard_deviation/
-
-    >>> Stats(range(100_000))
-    Stats:
-        100,000 samples, Σ=4,999,950,000
-        first=0, last=99,999
-        min=0, max=99,999
-        x̄=5e+04, s=2.89e+04
-
-    >>> s = Stats([0]); s
-    Stats:
-        1 sample, Σ=0
-        first=0, last=0
-        min=0, max=0
-        x̄=0, s=0
-    >>> s.add(-2.0); s
-    Stats:
-        2 samples, Σ=-2.0
-        first=0, last=-2.0
-        min=-2.0, max=0
-        x̄=-1, s=1.41
-
-    >>> Stats([])
-    Traceback (most recent call last):
-      ...
-    StopIteration
-    """
-    things = (
-        'first',
-        'last',
-        'min',
-        'max',
-        'sum',
-        'mean',
-    )
-    __slots__ = things + (
-        'count',
-        'ssdm',  # Sum of squared deviations from the mean.
-    )
-
-    def __init__(self, samples):
-        it = iter(samples)
-        sample = next(it)
-        for attr in self.things:
-            setattr(self, attr, sample)
-        self.count = 1
-        self.ssdm = 0
-        self.update(it)
-
-    def add(self, sample):
-        """Update stats with a new sample."""
-        self.last = sample
-        self.count += 1
-        self.sum += sample
-        if sample < self.min:
-            self.min = sample
-        elif sample > self.max:
-            self.max = sample
-
-        diff = sample - self.mean
-        self.mean += diff / self.count
-        # Compute additional squared deviation using
-        # both the previous and current means.
-        self.ssdm += diff * (sample - self.mean)
-
-    def update(self, samples):
-        """Update stats with multiple samples."""
-        sample = self.last
-        smallest = self.min
-        largest = self.max
-        total = self.sum
-        mean = self.mean
-        count = self.count
-        ssdm = self.ssdm
-
-        for sample in samples:
-            count += 1
-            total += sample
-            if sample < smallest:
-                smallest = sample
-            elif sample > largest:
-                largest = sample
-
-            diff = sample - mean
-            mean += diff / count
-            # Compute additional squared deviation using
-            # both the previous and current means.
-            ssdm += diff * (sample - mean)
-
-        self.last = sample
-        self.min = smallest
-        self.max = largest
-        self.sum = total
-        self.mean = mean
-        self.count = count
-        self.ssdm = ssdm
-
-        if __debug__:
-            if self.precision_loss > 1e-6:
-                warnings.warn(f"Precision loss of {self.precision_loss}")
-
-    @property
-    def variance(self):
-        return variance(self.ssdm, self.count)
-
-    @property
-    def stdev(self):
-        return stdev(self.ssdm, self.count)
-
-    @property
-    def precision_loss(self):
-        return abs(self.sum / self.count - self.mean)
-
-    def _update_slow(self, samples):
-        """Same as above, but about half as fast for a list of 1M ints.
-
-        Sure looks a lot prettier, though.
-        """
-        for sample in samples:
-            self.count += 1
-            self.sum += sample
-            self.min = min(self.min, sample)
-            self.max = max(self.max, sample)
-
-            diff = sample - self.mean
-            self.mean += diff / self.count
-            # Compute additional squared deviation using
-            # both the previous and current means.
-            self.ssdm += diff * (sample - self.mean)
-
-        self.last = sample
-
-        assert self.sum / self.count - self.mean < 1e-6
-
-    def __repr__(self):
-        s = 's' if self.count > 1 else ''
-        return "{}:\n{}".format(
-            type(self).__name__,
-            indented_lines(
-                f"{self.count:,} sample{s}, Σ={self.sum:,}",
-                f"first={self.first:,}, last={self.last:,}",
-                f"min={self.min:,}, max={self.max:,}",
-                f"x̄={self.mean:,.3g}, s={self.stdev:,.3g}",
-            ),
-        )
-
-    # TODO: __format__
-    # >>> import datetime
-    # >>> d = datetime.datetime(2010, 7, 4, 12, 15, 58)
-    # >>> '{:%Y-%m-%d %H:%M:%S}'.format(d)
-    # '2010-07-04 12:15:58'
-
-    # TODO: __add__, __or__, __iadd__, etc
-
-    @property
-    def x̄(self):
-        return self.mean
-
-    @property
-    def s(self):
-        return self.stdev
-
-    @property
-    def s2(self):
-        return self.variance
-
-    @property
-    def Σs2(self):
-        return self.ssdm
-
-
-def stats(samples):
-    """Compute statistics in a single pass over the samples.
-
-    Returns a dict with the following keys:
-
-        count, first, last, smallest, largest, total, mean, variance
+class Stats(namedtuple('Stats', [
+    'count',
+    'first',
+    'last',
+    'min',
+    'max',
+    'sum',
+    'mean',
+    'ssdm',  # Sum of squared deviations from the mean.
+])):
+    """Calculate running statistics in a single pass over a sequence.
 
     Uses Welford's method with Bessel's correction for computing the
     unbiased sample variance.
@@ -221,50 +23,138 @@ def stats(samples):
 
     Example:
 
-        >>> pstats([1, 2, 3])
-        count: 3
-        first: 1
-        last: 3
-        smallest: 1
-        largest: 3
-        total: 6
-        mean: 2.0
-        variance: 1.0
+        >>> Stats.of([1, 2, 3]).pprint()
+        Stats(
+            count=3,
+            first=1,
+            last=3,
+            min=1,
+            max=3,
+            sum=6,
+            mean=2.0,
+            ssdm=2.0,
+            # variance: 1.0
+            # stdev: 1.0
+        )
 
-    Note that the unbiased sample variance is 1.0, not 2/3.
+    (Note that the unbiased sample variance is 1.0, not 2/3.)
 
+    Stats can be updated with any iterable:
+
+        >>> Stats.of([1]) + [2] + iter([3])
+        Stats(count=3, first=1, last=3, min=1, max=3, sum=6, mean=2.0, ssdm=2.0)
+
+    Stats instances are immutable; in-place addition is reassignment:
+
+        >>> s1 = s2 = Stats.of(b'ayy')
+        >>> s2 += b'lmao'
+        >>> assert s1 != s2
+
+    Stats instances can be combined (including their variance!):
+
+        >>> combined = Stats.of([1]) | Stats.of([2]) | Stats.of([3])
+        >>> assert combined == Stats.of([1, 2, 3])
     """
-    samples = iter(samples)
-    count = 1
-    first = last = smallest = largest = total = mean = next(samples)
-    ssdm = 0  # Sum of squared deviations from the mean.
 
-    prev_dev = 0  # This gets clobbered *iff* samples isn't empty.
-    for last in samples:
-        count += 1
-        total += last
-        if last < smallest:
-            smallest = last
-        elif last > largest:
-            largest = last
+    @classmethod
+    def of(cls, samples):
+        samples = iter(samples)
+        first = last = min = max = sum = mean = next(samples)
+        self = cls(1, first, last, min, max, sum, mean, 0)
+        return self + samples
 
-        prev_dev = last - mean
-        mean += prev_dev / count
-        # Welford's method: compute additional squared deviation using
-        # the deviation from both the previous and current means.
-        ssdm += prev_dev * (last - mean)
+    def __add__(self, samples):
+        """Update the Stats with additional samples."""
+        assert not isinstance(samples, Stats), "merge Stats with |, not +"
+        count, first, last, min, max, sum, mean, ssdm = self
 
-    try:
-        # Bessel's correction: use n - 1 to reduce sample bias.
-        variance = ssdm / (count - 1)
-    except ZeroDivisionError:
-        variance = 0.0
+        for last in samples:
+            count += 1
+            sum += last
+            if last < min:
+                min = last
+            elif last > max:
+                max = last
 
-    del samples, prev_dev, ssdm
-    return locals()
+            prev_dev = last - mean
+            mean += prev_dev / count
+            # Welford's method: compute additional squared deviation using
+            # the deviation from both the previous and current means.
+            ssdm += prev_dev * (last - mean)
 
+        return self.__class__(count, first, last, min, max, sum, mean, ssdm)
 
-def pstats(samples):
-    """Compute and print statistics about the samples."""
-    for k, v in stats(samples).items():
-        print(f'{k}: {v}')
+    def merge(*stats):
+        """Merge the data from multiple Stats instances."""
+        assert stats
+        count = sum(s.count for s in stats)
+        total = sum(s.sum for s in stats)
+        combined_mean = total / count
+        combined_ssdm = sum(s._ssdm(combined_mean) for s in stats)
+
+        first, *_, last = stats
+
+        return first.__class__(
+            count=count,
+            # Assume the Stats objects were provided in order.
+            first=first.first,
+            last=last.last,
+            min=min(s.min for s in stats),
+            max=max(s.max for s in stats),
+            sum=total,
+            mean=combined_mean,
+            ssdm=combined_ssdm,
+        )
+
+    def _ssdm(self, combined_mean):
+        """Partial sum of squared deviations from the combined mean."""
+        return self.ssdm + self.count * (self.mean - combined_mean) ** 2
+
+    __or__ = merge
+
+    def __eq__(self, other):
+        """Check for equality within the bounds of floating point precision."""
+        if not isinstance(other, Stats):
+            return NotImplemented
+        return all(isclose(s, o) for s, o in zip(self, other))
+
+    @property
+    def variance(self):
+        """Unbiased sample variance, using Bessel's correction.
+
+        See https://en.wikipedia.org/wiki/Bessel%27s_correction
+        """
+        try:
+            # Bessel's correction: use n - 1 to reduce sample bias.
+            return self.ssdm / (self.count - 1)
+        except ZeroDivisionError:
+            return 0.0
+
+    @property
+    def stdev(self):
+        """Standard deviation (using the unbiased sample variance)."""
+        return self.variance ** 0.5
+
+    @property
+    def precision_loss(self):
+        return abs(self.sum / self.count - self.mean)
+
+    if __debug__:  # Only check precision in non-optimized mode.
+        def __init__(self, *args, **kwargs):
+            if self.precision_loss > 1e-6:
+                warnings.warn(f"precision loss of {self.precision_loss:.1e}")
+
+    @property
+    def pretty(self):
+        return '\n'.join(self._pretty())
+
+    def _pretty(self):
+        yield f"{self.__class__.__name__}("
+        for name, value in zip(self._fields, self):
+            yield f"    {name}={value},"
+        yield f"    # variance: {self.variance}"
+        yield f"    # stdev: {self.stdev}"
+        yield ")"
+
+    def pprint(self):
+        print(self.pretty)
--- stats.rst
+++ stats.rst
@@ -9,28 +9,96 @@ Stats are computed quickly, even over 1 million samples:
 
     >>> from time import perf_counter as clock
     >>> start = clock()
-    >>> pstats(range(1_000_000))
-    count: 1000000
-    first: 0
-    last: 999999
-    smallest: 0
-    largest: 999999
-    total: 499999500000
-    mean: 499999.5
-    variance: 83333416666.66666
+    >>> s = Stats.of(range(1_000_000))
+    >>> s.pprint()
+    Stats(
+        count=1000000,
+        first=0,
+        last=999999,
+        min=0,
+        max=999999,
+        sum=499999500000,
+        mean=499999.5,
+        ssdm=8.333333333324998e+16,
+        # variance: 83333416666.66666
+        # stdev: 288675.27893234405
+    )
     >>> end = clock()
     >>> assert end - start < 0.5
 
 
+Addition works as expected:
+
+    >>> s = Stats.of([0])
+    >>> s
+    Stats(count=1, first=0, last=0, min=0, max=0, sum=0, mean=0, ssdm=0)
+
+    >>> s + [1]
+    Stats(count=2, first=0, last=1, min=0, max=1, sum=1, mean=0.5, ssdm=0.5)
+
+    >>> s += [1]
+    >>> s
+    Stats(count=2, first=0, last=1, min=0, max=1, sum=1, mean=0.5, ssdm=0.5)
+
+
+Works with sum():
+
+    >>> sum([[2], [3]], s)
+    Stats(count=4, first=0, last=3, min=0, max=3, sum=6, mean=1.5, ssdm=5.0)
+
+
+In-place addition reassigns, and doesn't mutate:
+
+    >>> s0 = s1 = Stats.of([0])
+    >>> assert s1 is s0
+    >>> assert s1 == s0
+    >>> s1 += [0]
+    >>> assert s1 is not s0  # s1 has been reassigned.
+    >>> assert s1 != s0  # Its value has changed.
+
+
+Empty samples don't change any values:
+
+    >>> s0 = s1 = Stats.of([0])
+    >>> assert s1 is s0
+    >>> assert s1 == s0
+    >>> s1 += ()
+    >>> assert s1 is not s0  # s1 has been reassigned.
+    >>> assert s1 == s0  # Its value has *not* changed.
+    >>> assert repr(s1) == repr(s0)  # mean and ssdm are still ints.
+
+
 Miscellaneous corner cases:
 
-    >>> stats([0]).values()
-    dict_values([1, 0, 0, 0, 0, 0, 0, 0.0])
+    >>> Stats.of([0]).pprint()
+    Stats(
+        count=1,
+        first=0,
+        last=0,
+        min=0,
+        max=0,
+        sum=0,
+        mean=0,
+        ssdm=0,
+        # variance: 0.0
+        # stdev: 0.0
+    )
 
-    >>> stats([1]).values()
-    dict_values([1, 1, 1, 1, 1, 1, 1, 0.0])
+    >>> Stats.of([1]).pprint()
+    Stats(
+        count=1,
+        first=1,
+        last=1,
+        min=1,
+        max=1,
+        sum=1,
+        mean=1,
+        ssdm=0,
+        # variance: 0.0
+        # stdev: 0.0
+    )
 
-    >>> stats([])
+    >>> Stats.of([])
     Traceback (most recent call last):
       ...
     StopIteration
