b6a745dd5334bed7fe7f792b8b8d1c91105c6f0e
Frederick Wagner <doctaphred@gmail.com>
2021-07-25 19:36:44 -0400

    Don't alphabetize fields; clarify tests

--- records.py
+++ records.py
@@ -2,57 +2,58 @@
 
 Example usage:
 
-    >>> r1 = record(ayy='lmao', foo='bar')
-    >>> r2 = record(ayy='lmao', foo='bar')
+    >>> r_orig = record(foo='ayy', bar='lmao')
+    >>> r_same = record(foo='ayy', bar='lmao')
 
-    >>> r1
-    record(ayy='lmao', foo='bar')
-    >>> r2
-    record(ayy='lmao', foo='bar')
+    >>> r_orig
+    record(foo='ayy', bar='lmao')
+    >>> r_same
+    record(foo='ayy', bar='lmao')
 
-    >>> r1 == r2
+    >>> r_orig == r_same
     True
 
 Records with the same fields, in the same order, have the same type:
 
-    >>> type(r1) is type(r2)
+    >>> type(r_orig) is type(r_same)
     True
 
 Records are hashable:
 
-    >>> r1 in {r1}
+    >>> r_orig in {r_orig}
     True
 
-    >>> r1 in {r2}
+    >>> r_orig in {r_same}
     True
 
 Records with different values are not equal, but still have the same type:
 
-    >>> r3 = record(ayy='lmao', foo='baz')
+    >>> r_diff = record(foo='ayy', bar='wat')
 
-    >>> r1 == r3 or r2 == r3
+    >>> r_orig == r_diff or r_same == r_diff
     False
-    >>> r3 in (r1, r2)
+    >>> r_diff in (r_orig, r_same)
     False
-    >>> type(r1) is type(r3)
+    >>> type(r_orig) is type(r_diff)
     True
 
 Different field orders result in different, inequivalent types:
 
-    >>> r4 = record(foo='baz', ayy='lmao')
+    >>> r_forward = record(foo='ayy', bar='lmao')
+    >>> r_reverse = record(bar='lmao', foo='ayy')
 
-    >>> r3.ayy == r4.ayy and r3.foo == r4.foo
+    >>> r_forward.foo == r_reverse.foo and r_forward.bar == r_reverse.bar
     True
 
-    >>> r3 == r4
+    >>> r_forward == r_reverse
     False
 
-    >>> type(r4) is type(r3)
+    >>> type(r_reverse) is type(r_forward)
     False
 
 Instances are cached:
 
-    >>> r1 is r2
+    >>> r_orig is r_same
     True
 
 Caching is implemented via weakrefs, so instances are destroyed when no
@@ -82,24 +83,24 @@ evaluation of certain expressions:
 
 Derivative records can be created by calling instances:
 
-    >>> r5 = r1(foo='baz')
-    >>> r5
-    record(ayy='lmao', foo='baz')
+    >>> r_derived = r_orig(bar='wat')
+    >>> r_derived
+    record(foo='ayy', bar='wat')
 
 Caching still applies to new records created this way:
 
-    >>> r5 is r3
+    >>> r_derived is r_diff
     True
 
 Records have no __dict__, but can easily create one:
 
-    >>> vars(r1)
+    >>> vars(r_orig)
     Traceback (most recent call last):
       ...
     TypeError: vars() argument must have __dict__ attribute
 
-    >>> dict(r1)
-    {'ayy': 'lmao', 'foo': 'bar'}
+    >>> dict(r_orig)
+    {'foo': 'ayy', 'bar': 'lmao'}
 """
 from weakref import WeakValueDictionary
 
@@ -168,7 +169,7 @@ class record(metaclass=RecordMeta):
 
     def __iter__(self):
         """Iterate over key-value PAIRS, as G-d intended."""
-        for name in dir(self):
+        for name in self.__class__.__slots__:
             if not name.startswith('_'):
                 yield name, getattr(self, name)
 
